6dbd59b552e439270638432c5cfaf26f264978f5
mstem <stempeck@gmail.com>
2018-03-09 14:24:39 -0500

    repointed file directories
    Organized files into common directories without breaking everything
    
    Path to the-emoji-compass/www/platform_www/cordova.js still doesn't resolve

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,32 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+const fs = require('fs')
+const bs = require('browser-sync').create()
+const sass = require('node-sass')
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+bs.init({
+  server: './',
+  files: [
+    'index.html',
+    'scripts.js',
+    'styles.css',
+    '*.png',
+    {
+      match: 'styles.scss',
+      fn: function (event, file) {
+        // windows systems has trouble reading edited scss files sometimes
+        // this hack with setTimeout seems to work. see here:
+        // https://github.com/sass/node-sass/issues/1894#issuecomment-309051121
+        setTimeout(function () {
+          sass.render({
+            file: file,
+            outFile: 'styles.css'
+          }, function (err, result) {
+            if (err) return console.error(err)
+            fs.writeFile('styles.css', result.css, function (err) {
+              if (err) return console.error(err)
+            })
+          })
+        }, 100)
+      }
     }
-};
+  ]
+})
