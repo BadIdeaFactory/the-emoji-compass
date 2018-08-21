43096ff2d969083ee7c81ab266c9b74b716eaef8
Lou Huang <lou@louhuang.com>
2018-08-21 19:27:08 -0400

    utils: finish tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -29,3 +29,40 @@ describe('getEmojiPosition()', () => {
     expect(result).toEqual(0)
   })
 })
+
+describe('getUniqueRandomIntegers', () => {
+  it('generates 10 random numbers between 0 and 3', () => {
+    const ARRAY_LENGTH = 10
+    const UPPER_BOUND = 3
+    const result = getUniqueRandomIntegers(UPPER_BOUND, ARRAY_LENGTH)
+    expect(Array.isArray(result)).toEqual(true)
+    expect(result.length).toEqual(ARRAY_LENGTH)
+
+    // We need to test each value of the resulting array to make sure
+    // it is greater than or equal to 0, and less than the upper bound.
+    // We use Array.prototype.reduce() to check each value. If a value
+    // is found to be outside the bounds, return false, and continue to
+    // return false for the duration of the reduce().
+    let isInBounds = true
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] < 0 || result[i] >= UPPER_BOUND) {
+        isInBounds = false
+        break
+      }
+    }
+
+    expect(isInBounds).toEqual(true)
+  })
+})
+
+describe('getRotation', () => {
+  it('gets the rotation', () => {
+    const result = getRotation(4, 36)
+    expect(result).toEqual(40)
+  })
+
+  it('gets the rotation when position is zero', () => {
+    const result = getRotation(0, 36)
+    expect(result).toEqual(0)
+  })
+})
