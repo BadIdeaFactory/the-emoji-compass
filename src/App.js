789eb4a916c28e34c09f0f77e1af73b513171d5e
Lou Huang <lou@louhuang.com>
2018-08-21 21:13:22 -0400

    In progress: port rest of app to React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
 import React, { Component } from 'react'
 
+const ROUTES = {
+  MAIN: 'MAIN',
+  ANSWER: 'ANSWER'
+}
+
 class App extends Component {
+  constructor(props) {
+    this.state = {
+      route: ROUTE.MAIN,
+      requestEmojis: [],
+      responseEmojis: []
+    }
+  }
+
+  resetToInitialState = () => {
+    this.setState({ route: ROUTE.MAIN })
+  }
+
+  renderAnswerScreen = () => {
+    if (this.state.route === ROUTE.ANSWER) {
+      return (
+        <AnswerScreen
+          handleAskAnother={resetToInitialState}
+          requestEmojis={requestEmojis}
+          responseEmojis={responseEmojis}
+        />
+      )
+    }
+  }
+
   render () {
-    return null
+    return (
+      <div id="main">
+        <div class="container">
+          <div id="compass" class="compass-container">
+            <div class="ring-container">
+              <ul id="ring" class="ring"></ul>
+            </div>
+            <div class="dials-container">
+              <div id="dials">
+                <div class="dials-cap"></div>
+              </div>
+            </div>
+          </div>
+          <div class="text-container">
+            <hr />
+            <div class="text-box instruction-text">
+            </div>
+            <div class="text-box flavor-text hidden">
+              <div id="emoji-output"></div>
+              <div id="flavor-text-output"></div>
+            </div>
+          </div>
+        </div>
+        {this.renderAnswerScreen()}
+      </div>
+    )
   }
 }
 
