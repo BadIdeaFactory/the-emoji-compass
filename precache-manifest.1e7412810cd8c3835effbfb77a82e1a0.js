e20321b59e00df71bd1db2a8a65efe9e67975ff6
Lou Huang <lou@louhuang.com>
2018-10-23 16:37:36 +0200

    Updates

---
+++ precache-manifest.1e7412810cd8c3835effbfb77a82e1a0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dd1b76a746fb41af03bb646fca0d5b5",
+    "url": "/the-emoji-compass/build/static/media/GentiumBookBasic.3dd1b76a.woff2"
+  },
+  {
+    "revision": "bd6eba973fcd0464dea9",
+    "url": "/the-emoji-compass/build/static/css/main.f85223e6.chunk.css"
+  },
+  {
+    "revision": "ec31b34822e4817ca96c",
+    "url": "/the-emoji-compass/build/static/js/1.ec31b348.chunk.js"
+  },
+  {
+    "revision": "a536707c899ea23225ab",
+    "url": "/the-emoji-compass/build/static/js/runtime~main.a536707c.js"
+  },
+  {
+    "revision": "152f5c9852f5fa4a5d12c995bce97613",
+    "url": "/the-emoji-compass/build/static/media/alphaomega.152f5c98.svg"
+  },
+  {
+    "revision": "185b9c3448b013ebeb3845c010f23bb8",
+    "url": "/the-emoji-compass/build/static/media/share_button_v01.185b9c34.svg"
+  },
+  {
+    "revision": "234a91ed93eb5ef3f1a9249bf6d28db0",
+    "url": "/the-emoji-compass/build/static/media/close_button_v01.234a91ed.svg"
+  },
+  {
+    "revision": "11d34872ca11a0411e242cb183152d88",
+    "url": "/the-emoji-compass/build/static/media/compass_v01.11d34872.svg"
+  },
+  {
+    "revision": "bd6eba973fcd0464dea9",
+    "url": "/the-emoji-compass/build/static/js/main.bd6eba97.chunk.js"
+  },
+  {
+    "revision": "e22a9a654fb30c0cfe84965fccd68dee",
+    "url": "/the-emoji-compass/build/static/media/BerkshireSwash-Regular.e22a9a65.woff2"
+  },
+  {
+    "revision": "6c7eb4762771218552f2cfab4f6ea837",
+    "url": "/the-emoji-compass/build/static/media/GentiumBookBasic-Bold.6c7eb476.woff2"
+  },
+  {
+    "revision": "729fa70f2f455458e026d3686f2b3833",
+    "url": "/the-emoji-compass/build/static/media/clock-hand-active.729fa70f.png"
+  },
+  {
+    "revision": "7bcc6c27fcf83e299b437fbf807feb35",
+    "url": "/the-emoji-compass/build/static/media/clock-hand-inactive.7bcc6c27.png"
+  },
+  {
+    "revision": "ad933b4a6257a91f5574913093bb323f",
+    "url": "/the-emoji-compass/build/static/media/clock-hand2-inactive.ad933b4a.png"
+  },
+  {
+    "revision": "fbf927523ef72f12fb15cb1589aadb05",
+    "url": "/the-emoji-compass/build/static/media/clock-hand2-active.fbf92752.png"
+  },
+  {
+    "revision": "419516366c90ae90963f9bcec94329d5",
+    "url": "/the-emoji-compass/build/static/media/aurora_v04.41951636.svg"
+  },
+  {
+    "revision": "e271fff19220acbf9b936d783e3f37af",
+    "url": "/the-emoji-compass/build/index.html"
+  }
+];
