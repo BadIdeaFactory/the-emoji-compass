c5612e78fbcdf915981eb08cea6608d7b3ed31c7
Lou Huang <lou@louhuang.com>
2018-11-26 20:06:48 -0500

    MainTextDisplay: show response emojis when 4th hand is spinning

--- src/components/MainTextDisplay.js
+++ src/components/MainTextDisplay.js
@@ -8,7 +8,8 @@ import './MainTextDisplay.css'
 const TEXT_DISPLAY = {
   INSTRUCTION1: 'INSTRUCTION1',
   INSTRUCTION2: 'INSTRUCTION2',
-  EMOJI_DESCRIPTION: 'EMOJI_DESCRIPTION'
+  EMOJI_DESCRIPTION: 'EMOJI_DESCRIPTION',
+  RESPONSE_EMOJIS: 'RESPONSE_EMOJIS'
 }
 
 class MainTextDisplay extends React.Component {
@@ -19,7 +20,12 @@ class MainTextDisplay extends React.Component {
       text: PropTypes.string
     })),
     handPosition: PropTypes.number,
-    activeHand: PropTypes.number
+    activeHand: PropTypes.number,
+    responseEmojis: PropTypes.array
+  }
+
+  static defaultProps = {
+    responseEmojis: []
   }
 
   constructor (props) {
@@ -42,6 +48,12 @@ class MainTextDisplay extends React.Component {
     if (props.activeHand && props.activeHand > 1 && props.handPosition === null) {
       return {
         textDisplay: TEXT_DISPLAY.INSTRUCTION2
+      }
+    }
+
+    if (props.responseEmojis.length > 0) {
+      return {
+        textDisplay: TEXT_DISPLAY.RESPONSE_EMOJIS
       }
     }
 
@@ -74,6 +86,14 @@ class MainTextDisplay extends React.Component {
           </div>
         )
       }
+      case TEXT_DISPLAY.RESPONSE_EMOJIS:
+        return (
+          <div className="text-box emoji-picking">
+            {this.props.responseEmojis.map((emoji, i) => (
+              <Emoji symbol={emoji} key={i} />
+            ))}
+          </div>
+        )
       case TEXT_DISPLAY.INSTRUCTION2:
         return (
           <div className="text-box instruction-text">
@@ -103,7 +123,8 @@ function mapStateToProps (state) {
   return {
     symbols: state.app.symbols,
     handPosition: state.app.handPosition,
-    activeHand: state.app.activeHand
+    activeHand: state.app.activeHand,
+    responseEmojis: state.app.responseEmojis
   }
 }
 
