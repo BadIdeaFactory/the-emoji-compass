7208f4bc81145e52b9b0f3df5844ca4c2e13777f
Lou Huang <lou@louhuang.com>
2019-11-07 15:52:38 -0500

    feat: introduce sentry.io bug tracking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import * as Sentry from '@sentry/browser'
 import App from './App'
-import { unregister } from './registerServiceWorker'
 
 import './styles/typography.css'
 import './styles/styles.css'
@@ -10,11 +10,10 @@ import './styles/styles.css'
 // Redux
 import store from './store'
 
+Sentry.init({dsn: "https://dc3d74cf37114918b386e2c1152ac3f6@sentry.io/1811760"})
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('root'))
-
-// registerServiceWorker()
-unregister()
