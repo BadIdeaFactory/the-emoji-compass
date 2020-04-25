500dc8583f490e537dd88f033f71f05a9c4aa46b
Lou Huang <lou@louhuang.com>
2020-04-24 23:15:35 -0400

    feat(pwa): turn on service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import * as Sentry from '@sentry/browser'
+import * as serviceWorker from './serviceWorker'
 import App from './App'
 
 import './styles/typography.css'
@@ -17,3 +18,8 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
